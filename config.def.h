835aeb8a1e4ca76c757f05ddf145e13295f93687
hmpthcs <106286734+hmpthcs@users.noreply.github.com>
2024-09-21 20:49:25 +0000

    Try better default colors for EPD

--- config.def.h
+++ config.def.h
@@ -1,9 +1,19 @@
 #ifndef config_def_h_INCLUDED
 #define config_def_h_INCLUDED
 
-#define DEFAULT_FONT "Sans 14"
-#define DEFAULT_ROUNDING 5
+#define DEFAULT_FONT "Bitter 14"
+#define DEFAULT_ROUNDING 3
 static const int transparency = 255;
+
+/* wvkbd-mobintl --fg ffffffff --fg-sp ccccccff \ 
+--bg 000000aa --text 000000ff --text-sp 000000ff \
+--press 000000aa --press-sp 000000aa --swipe 000000aa \
+--swipe-sp 000000aa -L 160 -H 180 \
+--landscape-layers landscape,special -l simple,special \
+--hidden --fn "Bitter 14" -O \
+  | swipeGuess /usr/local/bin/count1w.txt \
+  | completelyTypeWord.sh &
+*/
 
 struct clr_scheme schemes[] = {
 {
@@ -18,10 +28,10 @@ struct clr_scheme schemes[] = {
 },
 {
   /* colors */
-  .bg = {.bgra = {15, 15, 15, transparency}},
-  .fg = {.bgra = {32, 32, 32, transparency}},
+  .bg = {.bgra = {0, 0, 0, transparency}},
+  .fg = {.bgra = {255, 255, 255, transparency}},
   .high = {.bgra = {100, 100, 100, transparency}},
-  .swipe = {.bgra = {100, 255, 100, 64}},
+  .swipe = {.bgra = {150, 150, 150, 64}},
   .text = {.color = UINT32_MAX},
   .font = DEFAULT_FONT,
   .rounding = DEFAULT_ROUNDING,
